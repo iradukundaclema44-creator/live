860c6cb302908d6c409db4ef9b6f1aebc74e7fdd
clema <munezerosonia85@gmail.com>
2026-09-16 17:16:39 +0200

    fhiiii

---
+++ booked.js
@@ -0,0 +1,25 @@
+
+// you are checking in the hotel is book if
+// booked then inform the customer that the
+// rooms are fully booked and if not booked then
+// inform them that the rooms are available with the price of the room
+//const rooms=100;
+//let booked=100;
+//let price=5000;
+
+//if (booked > rooms){
+ // console.log("rooms are fully booked")
+//}
+//else{
+  //console.log(" rooms are available and price is " + price);
+//}
+
+// let booked = true;
+let booked=false;
+let price=5000;
+if (booked== true){
+    console.log("the hotel are fully booked");
+}
+else{
+    console.log("the hotel rooms are available with the price of " +price +"$")
+}
